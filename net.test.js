f63d79c962dd925ca7f4a9b36df0794b8ba0e9be
migbaranes <migbaranes@gmail.com>
2024-06-25 12:34:14 +0300

    Happy&sad path test added

--- net.test.js
+++ net.test.js
@@ -1,55 +1,72 @@
 const { clickElement, putText, getText } = require("./lib/commands.js");
-const { generateName } = require("./lib/util.js");
+// const { generateName } = require("./lib/util.js");
+
+const { expect } = require("chai");
+
+// let page;
+
+// beforeEach(async () => {
+//   page = await browser.newPage();
+//   await page.goto("https://qamid.tmweb.ru/client/index.php");
+//   // await page.setDefaultNavigationTimeout(0);
+// });
+
+// afterEach(() => {
+//   page.close();
+// });
 
 let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
+  await page.goto("https://qamid.tmweb.ru/client/index.php");
 });
-
 afterEach(() => {
   page.close();
 });
 
-describe("Netology.ru tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://qamid.tmweb.ru/client/index.php");
-  });
-
-  test("The first test'", async () => {
-    const title = await page.title();
-    console.log("Page title: " + title);
-    await clickElement(page, "header a + a");
-    const title2 = await page.title();
-    console.log("Page title: " + title2);
-    const pageList = await browser.newPage();
-    await pageList.goto("https://netology.ru/navigation");
-    await pageList.waitForSelector("h1");
-  });
-
-  test("The first link text 'Медиа Нетологии'", async () => {
-    const actual = await getText(page, "header a + a");
-    expect(actual).toContain("Медиа Нетологии");
-  });
-
-  test("The first link leads on 'Медиа' page", async () => {
-    await clickElement(page, "header a + a");
-    const actual = await getText(page, ".logo__media");
-    await expect(actual).toContain("Медиа");
-  });
+describe("Happy path servise ИДЁМ В КИНО", () => {
+  test("Test1 'displaying the cost of booked tickets the film Mickey Mouse'", async () => {
+    await page.click(".movie-seances__time[href='#'][data-seance-id='199']");
+    await page.waitFor(1000);
+    await page.click('div:nth-child(5) span:nth-child(1)');
+    await page.click('div:nth-child(5) span:nth-child(2)');
+    await page.click('div:nth-child(6) span:nth-child(6)');
+    await page.click('div:nth-child(10) span:nth-child(8)');
+    await page.click(".acceptin-button");
+    await page.waitFor(1000);
+    const buttonText = 'body main p:nth-child(6)';
+    const actual = await page.$eval(buttonText, link => link.textContent);
+    expect(actual).equal('Стоимость: 2000 руб.');
+  }, 25000);  
 });
+  test("Test2 'displaying the cost of booked tickets Gone with the Wind'", async () => {
+    await page.click("a:nth-child(4)");
+    await page.click(".movie-seances__time[href='#'][data-seance-id='190']");
+    await page.waitFor(1000);
+    await page.click('div:nth-child(7) span:nth-child(5)');
+    await page.click('div:nth-child(5) span:nth-child(2)');
+    await page.click('div:nth-child(8) span:nth-child(6)');
+    await page.click('div:nth-child(4) span:nth-child(8)');
+    await page.click(".acceptin-button");
+    await page.waitFor(1000);
+    const buttonText = '.acceptin-button';
+    const actual = await page.$eval(buttonText, link => link.textContent);
+    expect(actual).equal("Получить код бронирования");
+  }, 25000);  
 
-test("Should look for a course", async () => {
-  await page.goto("https://netology.ru/navigation");
-  await putText(page, "input", "тестировщик");
-  const actual = await page.$eval("a[data-name]", (link) => link.textContent);
-  const expected = "Тестировщик ПО";
-  expect(actual).toContain(expected);
-});
-
-test("Should show warning if login is not email", async () => {
-  await page.goto("https://netology.ru/?modal=sign_in");
-  await putText(page, 'input[type="email"]', generateName(5));
+describe("Sad path servise ИДЁМ В КИНО", () => {
+  test("Test 'No booking button selector'", async () => {
+    await page.click(".movie-seances__time[href='#'][data-seance-id='199']");
+    await page.waitFor(1000);
+    await page.click('div:nth-child(5) span:nth-child(1)');
+    await page.click('div:nth-child(5) span:nth-child(2)');
+    await page.click('div:nth-child(5) span:nth-child(1)');
+    await page.click('div:nth-child(5) span:nth-child(2)');
+    await page.waitFor(1000);
+    const buttonText = 'body main p:nth-child(6)';
+    const actual = await page.$eval(buttonText, link => link.textContent);
+    expect(actual).equal('Стоимость: 1000 руб.');
+  }, 25000);  
+  
 });
