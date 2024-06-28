0eaf9af0c29939accafc21b4badeb1996afe5f06
migbaranes <migbaranes@gmail.com>
2024-06-28 12:53:46 +0300

    custom method added

--- net.test.js
+++ net.test.js
@@ -1,3 +1,5 @@
+const { clickElement, putText, getText } = require("./lib/commands.js");
+
 const { expect } = require("chai");
 let page;
 
@@ -12,12 +14,12 @@ afterEach(() => {
 describe("Happy path servise ИДЁМ В КИНО", () => {
   test("Test1 'displaying the cost of booked tickets the film Mickey Mouse'", async () => {
     await page.click(".movie-seances__time[href='#'][data-seance-id='199']");
-    await page.waitFor(1000);
+    await page.waitForSelector(".buying__info-hall");
     await page.click('div:nth-child(5) span:nth-child(1)');
     await page.click('div:nth-child(5) span:nth-child(2)');
     await page.click('div:nth-child(6) span:nth-child(6)');
     await page.click('div:nth-child(10) span:nth-child(8)');
-    await page.click(".acceptin-button");
+    await clickElement(page, ".acceptin-button");
     await page.waitFor(1000);
     const buttonText = 'body main p:nth-child(6)';
     const actual = await page.$eval(buttonText, link => link.textContent);
@@ -33,6 +35,8 @@ describe("Happy path servise ИДЁМ В КИНО", () => {
     await page.click('div:nth-child(8) span:nth-child(6)');
     await page.click('div:nth-child(4) span:nth-child(8)');
     await page.click(".acceptin-button");
+    const text = await getText(page, ".ticket__check-title");
+    await console.log(text);
     await page.waitFor(1000);
     const buttonText = '.acceptin-button';
     const actual = await page.$eval(buttonText, link => link.textContent);
@@ -40,7 +44,7 @@ describe("Happy path servise ИДЁМ В КИНО", () => {
   }, 25000);  
 
 describe("Sad path servise ИДЁМ В КИНО", () => {
-  test("Test 'No booking button selector'", async () => {
+  test.only("Test 'No booking button selector'", async () => {
     await page.click(".movie-seances__time[href='#'][data-seance-id='199']");
     await page.waitFor(1000);
     await page.click('div:nth-child(5) span:nth-child(1)');
